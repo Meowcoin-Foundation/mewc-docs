fbb17d989ac0f327ce38304de23e335938cbb4d6
Zach Price <zach.price105@gmail.com>
2025-03-30 01:11:42 -0700

    Update repository URLs in package.json and theme.config.tsx to reflect new project name 'mewc-docs'.

--- theme.config.tsx
+++ theme.config.tsx
@@ -9,7 +9,7 @@ const config: DocsThemeConfig = {
   chat: {
     link: 'https://discord.com/invite/meowcoin',
   },
-  docsRepositoryBase: 'https://github.com/meowcoin-foundation/documentation-meowcoin',
+  docsRepositoryBase: 'https://github.com/meowcoin-foundation/mewc-docs',
   footer: {
     text: 'Meowcoin Documentation',
   },
