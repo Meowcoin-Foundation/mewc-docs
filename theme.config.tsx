33c7958482e3601dd9786a8717ac31d53d3aa313
Zach Price <zach.price105@gmail.com>
2025-03-30 01:06:49 -0700

    Rename project to Meowcoin and update documentation, dependencies, and configuration files accordingly. Added new pages for mining, wallets, donations, and exchanges, while removing outdated pages. Enhanced README and theme configuration to reflect the new branding and mission focused on animal welfare.

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Meowcoin</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/meowcoin-foundation/documenation-meowcoin',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.com/invite/meowcoin',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/meowcoin-foundation/documentation-meowcoin',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Meowcoin Documentation',
   },
 }
 
